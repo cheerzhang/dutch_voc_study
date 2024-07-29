c8f5e19d63bb3f105d56633c846f357bb5cc609b
cheerzhang <zhanglenus@gmail.com>
2024-07-29 15:06:48 +0200

    Update words_action.py
    fix upper letter typo

--- utils/words_action.py
+++ utils/words_action.py
@@ -7,10 +7,10 @@ import os
 def load_data(username, role, data_type='NOUN'):
     if not os.path.exists("guest_NOUN.csv"):
         df = pd.DataFrame(columns=['word', 'plural', 'gender', 'translation_en', 'translation_zh', 'difficulty', 'search_count', 'admin_search_count'])
-        df.to_csv("guest_Noun.csv", index=False)
+        df.to_csv("guest_NOUN.csv", index=False)
     if not os.path.exists("guest_VERB.csv"):
         df = pd.DataFrame(columns=['verb', 'singular_present', 'plural_form', 'past_singular', 'past_plural', 'perfect_participle', 'translation_en', 'translation_zh', 'difficulty', 'search_count'])
-        df.to_csv("guest_Verb.csv", index=False)
+        df.to_csv("guest_VERB.csv", index=False)
     if role == 'guest':
         df = pd.read_csv(f"guest_{data_type}.csv")
     elif role == 'admin':
