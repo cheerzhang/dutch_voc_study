4953b5ddeb8f9bc7a5f679fab0173106ad7d9e45
cheerzhang <zhanglenus@gmail.com>
2024-07-29 17:29:46 +0200

    Update words_action.py

--- utils/words_action.py
+++ utils/words_action.py
@@ -16,9 +16,9 @@ def load_guest_data(data_type='NOUN'):
 def load_user_data(username, data_type='NOUN'):
     guest_df = load_guest_data(data_type)
     if not os.path.exists(f"{username}_{data_type}.csv"):
-        guest_df['admin_search_count'] = 0
         user_df = guest_df.copy()
-        user_df.to_csv(f"{username}_{data_type}.csv", index=False)
+        user_df['admin_search_count'] = 0
+    user_df.to_csv(f"{username}_{data_type}.csv", index=False)
     user_df = pd.read_csv(f"{username}_{data_type}.csv")
     return guest_df, user_df
     
